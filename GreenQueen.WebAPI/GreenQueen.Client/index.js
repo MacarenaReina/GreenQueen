3b4e39acb6f16b94549ada7ca2d7bc6457cb8d4a
AgoYaMo <agoyamo@gmail.com>
2017-03-01 18:20:54 +0000

    Cambios a la cadena de conexion para que funcione desde todos los PC Inicio de React (da error pues el servidor devuelve xml)

---
+++ GreenQueen.WebAPI/GreenQueen.Client/index.js
@@ -0,0 +1,42 @@
+﻿import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const baseUrl = 'http://localhost:50150';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      discs: null,
+      interpreters: null,
+      genres: null
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:50150/api/Discos")
+    .then(response => response.json())
+    .then(discs => this.setState({discs}));
+  }
+
+  render(){
+    const {discs, interpreters, genres} = this.state;
+    return(
+      <discList list={discs}/>
+    )
+  }
+}
+
+const discList = (list) => {
+  return(
+    <div>
+    {
+      list.map(item=>
+        <li class="list-group-item">{item.Titulo}</li>
+      )
+    }
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("listaDiscos"));
